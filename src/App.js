dc5e7d1acd8f24d09af5dbf854a9798e391c97ab
Taki <laszlo@letmewebyou.com>
2020-02-21 13:17:03 +0100

    Removed React logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './App.css'
-import logo from './logo.svg'
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 <p>SmarTender 2.0 is in progress...</p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
