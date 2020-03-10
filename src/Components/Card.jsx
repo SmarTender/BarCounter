ff1b3de7c1c864c1502de0d3d20351c0f4f5758e
Taki <laszlo@letmewebyou.com>
2020-03-10 14:48:31 +0100

    Added scss

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import './Card.scss'
 
 function Card(props) {
     return (
         <div className="card">
             <div className="top">
-                <img src={props.img} alt={props.drinkname} />
+                <div className="drink-image" style={{ backgroundImage: `url(${props.img})` }} alt={props.drinkname}>
+                    ""
+                </div>
             </div>
             <div className="bottom">
                 <h1>{props.drinkname}</h1>
